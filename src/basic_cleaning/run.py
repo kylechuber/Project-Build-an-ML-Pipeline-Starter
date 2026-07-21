36da78dd30e25ecf7cc40130e271f9cc2318b029
Kyle Huber <kylechuber@gmail.com>
2026-07-21 15:21:24 +0000

    Complete ML pipeline: cleaning, testing, splitting, training, and test steps

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -52,51 +52,44 @@ def go(args):
 # TODO: In the code below, fill in the data type for each argument. The data type should be str, float or int. 
 # TODO: In the code below, fill in a description for each argument. The description should be a string.
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser(description="A very basic data cleaning")
-  
-    parser.add_argument(
-        "--input_artifact", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
-        required = True
-    )
 
     parser.add_argument(
-        "--output_artifact", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
-        required = True
+        "--input_artifact",
+        type=str,
+        help="The name of the input artifact to clean (the raw sample data from W&B)",
+        required=True
     )
-
     parser.add_argument(
-        "--output_type", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
-        required = True
+        "--output_artifact",
+        type=str,
+        help="The name for the output artifact that will hold the cleaned data",
+        required=True
     )
-
     parser.add_argument(
-        "--output_description", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
-        required = True
+        "--output_type",
+        type=str,
+        help="The type to assign to the output artifact",
+        required=True
     )
-
     parser.add_argument(
-        "--min_price", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
-        required = True
+        "--output_description",
+        type=str,
+        help="A description of the output artifact",
+        required=True
     )
-
+    parser.add_argument(
+        "--min_price",
+        type=float,
+        help="The minimum price to keep; rows below this are dropped as outliers",
+        required=True
+    )
     parser.add_argument(
         "--max_price",
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
-        required = True
+        type=float,
+        help="The maximum price to keep; rows above this are dropped as outliers",
+        required=True
     )
-
 
     args = parser.parse_args()
 
--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -87,3 +87,15 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+def test_row_count(data: pd.DataFrame) -> None:
+    """
+    Test that the dataset has a reasonable number of rows (not too small, not too large).
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+
+def test_price_range(data: pd.DataFrame, min_price: float, max_price: float) -> None:
+    """
+    Test that all prices in the dataset fall within the expected min and max range.
+    """
+    assert data['price'].between(min_price, max_price).all()
